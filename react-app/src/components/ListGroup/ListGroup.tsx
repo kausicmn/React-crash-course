8796b8db9bb690209113f9d6a9b0ce37c934cb69
kausicmn <92553246+kausicmn@users.noreply.github.com>
2023-03-31 18:53:59 -0700

    ListGroup Vanila CSS Modules

--- react-app/src/components/ListGroup/ListGroup.tsx
+++ react-app/src/components/ListGroup/ListGroup.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import './ListGroup.css'
+import styles from './ListGroup.module.css'
 interface Props {
   items: string[];
   heading: string;
@@ -13,7 +13,7 @@ function ListGroup({ items, heading, onSelectItem}: Props) {
     <>
       <h1>{heading}</h1>
       {items.length === 0 && <p>No Item found</p>}
-      <ul className="list-group">
+      <ul className={[styles.listGroup,styles.container].join(' ')}>
         {items.map((item, index) => (
           <li
             className={
