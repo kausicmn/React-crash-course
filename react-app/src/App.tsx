e159319a931e0fef5ed267696127377b7f1fad31
kausicmn <92553246+kausicmn@users.noreply.github.com>
2023-04-01 23:33:05 -0700

    update array of objects using immer

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import ListGroup from "./components/ListGroup";
 import {AiFillChrome} from "react-icons/ai"
 import Like from "./components/Like/Like";
+import produce from 'immer'
 // import Button from "./components/Button";
 let items=["New york","SF","Chennai",'Chicago']
 // const handleClick=(item:String)=>{
@@ -11,10 +12,16 @@ function App() {
   const [alertVisible,setAlertVisibility]=useState(false);
   const [tags,setTags]=useState([{id:1,title:'Bug1',fixed:true},{id:2,title:'Bug2',fixed:false}]);
   const handleClick=()=>{
-    setTags(tags.map(tags=>tags.id===1?{...tags,fixed:false}:tags));
+    // setTags(tags.map(tags=>tags.id===1?{...tags,fixed:false}:tags));
+    setTags(produce(draft=>{
+      const bug=draft.find(tags=>tags.id===1);
+      if(bug) bug.fixed=false;
+
+    }))
   }
   return (
     <div>
+      {tags.map(tags=><p>{tags.title} cc{tags.id} {tags.fixed}</p>)}
     <button onClick={handleClick}>click here</button>
     </div>
   );
