4250760aba505863f1ae836518baab71e537b3f7
kausicmn <92553246+kausicmn@users.noreply.github.com>
2023-04-01 21:25:08 -0700

    Change object

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -2,17 +2,27 @@ import { useState } from "react";
 import ListGroup from "./components/ListGroup";
 import {AiFillChrome} from "react-icons/ai"
 import Like from "./components/Like/Like";
+// import Button from "./components/Button";
 let items=["New york","SF","Chennai",'Chicago']
-const handleClick=(item:String)=>{
-  console.log(item)
-}
+// const handleClick=(item:String)=>{
+//   console.log(item)
+// }
 function App() {
   const [alertVisible,setAlertVisibility]=useState(false);
+  const [drink,setdrink]=useState({
+    title:'Red bull',
+    price:5
+  });
+  const handleClick=()=>{
+    setdrink({...drink,price:6});
+  }
   return (
     <div>
-   <ListGroup items={items} heading='Cities' onSelectItem={handleClick}></ListGroup>
+   {/* <ListGroup items={items} heading='Cities' onSelectItem={handleClick}></ListGroup>
    <AiFillChrome></AiFillChrome>
-    <Like onClick={()=>{console.log('clicked')}}color='blue'></Like>
+    <Like onClick={()=>{console.log('clicked')}}color='blue'></Like> */}
+    {drink.price}
+    <button onClick={handleClick}>click here</button>
     </div>
   );
 }
