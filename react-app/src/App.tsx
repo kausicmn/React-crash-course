e8d97e46c014c84e8072cf40175d8ca0fb86c162
kausicmn <92553246+kausicmn@users.noreply.github.com>
2023-04-02 00:39:08 -0700

    Exercise 3

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -3,19 +3,21 @@ import Navbar from './components/Navbar'
 import Cart from './components/Cart'
 
 function App () {
-  const [items,setitems]=useState({
-    id:1,
-    player:{
-      name:"john",
-    },
+  const [Cart,setCart]=useState({
+    discount:.1,
+    items:[
+      {id:1,title:'Product 1',quantity:1},
+      {id:2,title:'Product 2',quantity:1},
+    ]
+    
   });
   const handleClick =() =>{
-    setitems({...items,player:{...items.player,name:"kausic"}})
+    setCart({...Cart,items:Cart.items.map(item=>item.id===1?{...item,quantity:item.quantity+1}:item)})
   }
   return (
     <>
     <div>App</div>
-    {items.player.name}
+    {Cart.items.map(item=><p>{item.id} {item.quantity} {item.title}</p>)}
     <button onClick={handleClick}>Click me</button>
     </>
   )
