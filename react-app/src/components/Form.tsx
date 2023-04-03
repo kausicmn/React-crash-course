cd17489ed9899d33db2dff2fcfaa54f1a45a6b16
kausicmn <92553246+kausicmn@users.noreply.github.com>
2023-04-02 20:33:29 -0700

    Zod resolver

--- react-app/src/components/Form.tsx
+++ react-app/src/components/Form.tsx
@@ -1,23 +1,27 @@
 import React, { FormEvent, useRef, useState } from 'react'
 import {FieldValues, useForm} from "react-hook-form"
-interface FormData{
-name:string
-age:number
-}
+import {z} from 'zod';
+import {zodResolver} from '@hookform/resolvers/zod'
+const schema=z.object({
+    name:z.string().min(3,{message:'Atleast 3 characters please'}),
+    age:z.number({invalid_type_error:'Age field is required'}).min(18)
+
+});
+type FormData=z.infer<typeof schema>;
 const Form = () => {
-    const {register,handleSubmit,formState:{errors}}=useForm<FormData>();   
+    const {register,handleSubmit,formState:{errors}}=useForm<FormData>({resolver:zodResolver(schema)});   
     const onSubmit = (data:FieldValues)=>console.log(data);
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
         <div className="mb-3">
             <label htmlFor="name" className="form-label form"></label>
-            <input {...register('name',{required:true,minLength:3})}id="name" type="text" className="form-control" />
-            {errors.name?.type==='required' && <p className='text-danger'>The name field is required</p>}
-            {errors.name?.type==='minLength' && <p className='text-danger'>Atleast 3 characters should be there</p>}
+            <input {...register('name')}id="name" type="text" className="form-control" />
+            {errors.name && <p className='text-danger'>{errors.name.message}</p>}
         </div>
         <div className="mb-3">
             <label htmlFor="age" className="form-label">Age</label>
-            <input {...register('age')}id="age" type="number" className="form-control" />
+            <input {...register('age',{valueAsNumber:true})}id="age" type="number" className="form-control" />
+            {errors.age && <p className='text-danger'>{errors.age.message}</p>}
         </div>
         <button className="btn btn-primary" type='submit'>Submit</button>
 
