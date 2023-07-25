57d5c35cb23bf63f2e5047c1819858813c68ecf6
kausicmn <92553246+kausicmn@users.noreply.github.com>
2023-07-25 16:16:08 -0500

    Cancelling fetch request abort controller

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,46 +1,42 @@
-import React, { useEffect, useRef, useState } from "react";
-import Navbar from "./components/Navbar";
-import Cart from "./components/Cart";
-import Expandable from "./components/Expandable";
-import Form from "./components/Form";
-import ExpenseForm from "./ExpenseTracker/components";
-import ExpenseFilter from "./ExpenseTracker/components/ExpenseFilter";
-import ExpenseList from "./ExpenseTracker/components/ExpenseList";
-import categories from "./ExpenseTracker/categories";
-import ProductList from "./components/ProductList";
-import axios, { AxiosError } from "axios";
-interface user {
-  id: string;
-  name: string;
+import React, { useEffect, useRef, useState } from 'react'
+import Navbar from './components/Navbar'
+import Cart from './components/Cart'
+import Expandable from './components/Expandable';
+import Form from './components/Form';
+import ExpenseForm from './ExpenseTracker/components';
+import ExpenseFilter from './ExpenseTracker/components/ExpenseFilter';
+import ExpenseList from './ExpenseTracker/components/ExpenseList';
+import categories from './ExpenseTracker/categories'
+import ProductList from './components/ProductList';
+import axios, { AxiosError, CanceledError } from 'axios';
+interface user
+{
+  id:string
+  name:string
 }
-function App() {
-  const [users, setusers] = useState<user[]>([]);
-  const [error, seterror] = useState("");
-  useEffect(() => {
-    const fetch = async () => {
-      try {
-        const res = await axios.get<user[]>(
-          "https://jsonplaceholder.typicode.com/users"
-        );
-        setusers(res.data);
-      } catch (err) {
-        seterror((err as AxiosError).message);
-      }
-    };
-    fetch();
-  }, []);
+function App () {
+const [users,setusers]=useState<user[]>([]);
+const [error,seterror]=useState('');
+useEffect(()=>{
+    const controller=new AbortController();
+    axios.get<user[]>("https://jsonplaceholder.typicode.com/users",{signal:controller.signal})
+    .then(res=>setusers(res.data)).catch((err)=>
+      {
+        if(err instanceof CanceledError) return;
+        seterror(err.message);
+    });
+    return () => controller.abort();
+},[])
   return (
     <>
-      {error && <p className="text-danger">{error}</p>}
-      <div>
-        <ul>
-          {users.map((user) => (
-            <li key={user.id}>{user.name}</li>
-          ))}
-        </ul>
-      </div>
+    {error&&<p className='text-danger'>{error}</p>}
+    <div>
+      <ul>
+        {users.map((user)=><li key={user.id}>{user.name}</li>)}
+      </ul>
+    </div>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
