7315f9309cc1665409b6e6b03d629d9b94055e4c
kausicmn <92553246+kausicmn@users.noreply.github.com>
2023-07-25 15:30:38 -0500

    Axios fetching data async await

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,35 +1,46 @@
-import React, { useEffect, useRef, useState } from 'react'
-import Navbar from './components/Navbar'
-import Cart from './components/Cart'
-import Expandable from './components/Expandable';
-import Form from './components/Form';
-import ExpenseForm from './ExpenseTracker/components';
-import ExpenseFilter from './ExpenseTracker/components/ExpenseFilter';
-import ExpenseList from './ExpenseTracker/components/ExpenseList';
-import categories from './ExpenseTracker/categories'
-import ProductList from './components/ProductList';
-import axios from 'axios';
-interface user
-{
-  id:string
-  name:string
+import React, { useEffect, useRef, useState } from "react";
+import Navbar from "./components/Navbar";
+import Cart from "./components/Cart";
+import Expandable from "./components/Expandable";
+import Form from "./components/Form";
+import ExpenseForm from "./ExpenseTracker/components";
+import ExpenseFilter from "./ExpenseTracker/components/ExpenseFilter";
+import ExpenseList from "./ExpenseTracker/components/ExpenseList";
+import categories from "./ExpenseTracker/categories";
+import ProductList from "./components/ProductList";
+import axios, { AxiosError } from "axios";
+interface user {
+  id: string;
+  name: string;
 }
-function App () {
-const [users,setusers]=useState<user[]>([]);
-const [error,seterror]=useState('');
-useEffect(()=>{
-  axios.get<user[]>("https://jsonplaceholder.typicode.com/ausers").then(res=>setusers(res.data)).catch(err=>seterror(err.message))
-},[])
+function App() {
+  const [users, setusers] = useState<user[]>([]);
+  const [error, seterror] = useState("");
+  useEffect(() => {
+    const fetch = async () => {
+      try {
+        const res = await axios.get<user[]>(
+          "https://jsonplaceholder.typicode.com/users"
+        );
+        setusers(res.data);
+      } catch (err) {
+        seterror((err as AxiosError).message);
+      }
+    };
+    fetch();
+  }, []);
   return (
     <>
-    {error&&<p className='text-danger'>{error}</p>}
-    <div>
-      <ul>
-        {users.map((user)=><li key={user.id}>{user.name}</li>)}
-      </ul>
-    </div>
+      {error && <p className="text-danger">{error}</p>}
+      <div>
+        <ul>
+          {users.map((user) => (
+            <li key={user.id}>{user.name}</li>
+          ))}
+        </ul>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
